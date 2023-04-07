ef269a9a913e3e326f0e6e7e609b0cc2bd5cf7af
Christoph Nientied <christophnientied@googlemail.com>
2023-04-07 13:14:18 +0200

    first try on creating mark as done btn

--- script.js
+++ script.js
@@ -4,43 +4,111 @@
 
 //WANN (Bei welchem Event soll etwas mit den Elementen passieren?)
 
-
-
-
-
 //Testing
 
 //WO
 
-let addBtn = document.getElementById('inputBtn');
-let inputField = document.getElementById('inputField');
-let outputField = document.getElementById('todoList');
-
+let addBtn = document.getElementById("inputBtn");
+let inputField = document.getElementById("inputField");
+let outputField = document.getElementById("todoList");
 
 //WAS
 function testing(e) {
-    e.preventDefault();
-    const newListItem = document.createElement('li');
-    newListItem.classList.add('todoListItem');
+  e.preventDefault();
+  const newListItem = document.createElement("li");
+  newListItem.classList.add("todoListItem");
+  //  newListItem.classList.add('todoListItemChecked')
 
-    const newSpanItem = document.createElement('span');
-    newSpanItem.classList.add('listValue');
-    newSpanItem.innerText = inputField.value;
+  const newSpanItem = document.createElement("span");
+  newSpanItem.classList.add("listValue");
+  newSpanItem.innerText = inputField.value;
 
-    const editBtn = document.createElement('button');
-    editBtn.classList.add('editBtn');
-    editBtn.textContent = 'Edit';
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("editBtn");
+  editBtn.textContent = "Edit";
 
-    const delBtn = document.createElement('button');
-    delBtn.classList.add('delBtn');
-    delBtn.textContent = 'X';
-    
-    newListItem.appendChild(newSpanItem);
-    newListItem.appendChild(editBtn);
-    newListItem.appendChild(delBtn);
-    
-    outputField.appendChild(newListItem);
+  const delBtn = document.createElement("button");
+  delBtn.classList.add("delBtn");
+  delBtn.textContent = "X";
+  // Alert eingebaut, wenn kein Wert in Inputfield eingegeben wurde.
+  if (!inputField.value) {
+    alert("Got nothing to add?");
+    return;
+  }
+  newListItem.appendChild(newSpanItem);
+  newListItem.appendChild(editBtn);
+  newListItem.appendChild(delBtn);
+
+  outputField.appendChild(newListItem);
 }
 
 //WANN
-addBtn.addEventListener('click', testing);
+addBtn.addEventListener("click", testing);
+
+///////////////////////////////////////////Mark as done /////////////////////////////////////////
+
+// Anmerkung: Auch bei Click auf Buttons wird die li eingefärbt, was mit den Funktionen für editieren und löschen kollodiert
+//WO: entweder li
+const todoList = document.querySelectorAll("ul");
+const todoValues = document.getElementsByTagName("li");
+const editBtn = document.querySelector(".editBtn");
+const delBtn = document.querySelector(".delBtn");
+
+//WAS: bg color dunkler und text soll durchgestrichen sein, ansonsten gleichen Styles wie .todoListItem
+
+// function markAsDone () {
+//     todoValues.forEach ((element) => {
+//         element.classList.toggle("todoListItem");
+//         element.classList.toggle("todoListItemChecked");
+
+//     })
+// }
+
+// function markAsDone () {
+//         todoValues.classList.toggle("todoListItem");
+//         todoValues.classList.toggle("todoListItemChecked");
+// }
+
+//WANN: bei click auf li
+// todoValues.addEventListener("click", markAsDone);
+
+//testing
+
+// todoValues.forEach(e => {
+//     e.addEventListener('click', event => {
+//       //handle click
+//       e.style.backgroundColor = "green";
+//       e.style.color = "white";
+//     })
+//   }
+//     )
+
+// function markAsDone () {
+//     todoValues.forEach(e => {
+//         e.classList.toggle("todoListItem");
+//         e.classList.toggle("todoListItemChecked");
+//     })
+// }
+
+//     todoValues.addEventListener("click", markAsDone)
+
+// todoValues.forEach(e => {
+//     e.addEventListener('click', event => {
+//       //handle click
+//         e.classList.toggle("todoListItem");
+//         e.classList.toggle("todoListItemChecked");
+//     })
+//   }
+//     )
+
+for (let todoValue of todoValues) {
+  todoValue.addEventListener("click", (event) => {
+    todoValue.classList.toggle("todoListItem");
+    todoValue.classList.toggle("todoListItemChecked");
+  });
+}
+
+
+//Unklar:
+// 1. Wie kann die Funktion für dynamisch erstellte, neue Elemente hinzugefügt werden?
+// 2. Wie können wir sicherstellen, dass die Buttons ausgenommen sind in der Funktion? Bislang ist bei Click auf beide Buttons alles ausgegraut?
