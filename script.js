ea40918422a1a499997554a7f2e9c1c68a6678f2
Salvatore <111086457+salvo081@users.noreply.github.com>
2023-04-10 22:15:43 +0200

    created functions like MVC pattern

--- script.js
+++ script.js
@@ -6,26 +6,71 @@
 
 
 
+//MODEL
+//nur das Array und die Functions verwenden
+//keine Elemente aus dem DOM verwenden
+//Model kennt das DOM nicht
+const todoList = [
+    // {id: '1', listValue: 'Apples'}
+];
 
+function generateId() {
+    //TODO generateId
+    return 'TESTID';
+}
 
-//Testing
+function getTodo(id) {
+    //TODO getTodo
+}
+
+function addTodo(listValue) {
+    const id = generateId();
+    const newTodoListObj = {id: id, listValue: listValue};
+
+    todoList.push(newTodoListObj);
+
+    return newTodoListObj;
+}
+
+function editTodo(id, listValue) {
+    //TODO editTodo
+}
+
+function deleteTodo(id) {
+    //TODO deleteTodo
+}
+
+function markAsDoneTodo(id) {
+    //TODO markAsDoneTodo
+}
+
+function saveTodoList() {
+    //TODO save to Local Storage
+}
+
+function loadTodoList() {
+    //TODO load from Local Storage
+}
+
+//VIEW
+//nur das DOM verwenden
+//keine Elemente aus dem Array verwenden
+//View kennt das Model nicht
 
 //WO
-
-let addBtn = document.getElementById('inputBtn');
-let inputField = document.getElementById('inputField');
-let outputField = document.getElementById('todoList');
-
+const inputForm = document.getElementById('inputForm');
+const inputField = document.getElementById('inputField');
+const outputField = document.getElementById('todoList');
 
 //WAS
-function testing(e) {
-    e.preventDefault();
+function createNewListItem(id, listValue) {
     const newListItem = document.createElement('li');
     newListItem.classList.add('todoListItem');
+    newListItem.dataset.id = id;
 
     const newSpanItem = document.createElement('span');
     newSpanItem.classList.add('listValue');
-    newSpanItem.innerText = inputField.value;
+    newSpanItem.innerText = listValue;
 
     const editBtn = document.createElement('button');
     editBtn.classList.add('editBtn');
@@ -38,9 +83,90 @@ function testing(e) {
     newListItem.appendChild(newSpanItem);
     newListItem.appendChild(editBtn);
     newListItem.appendChild(delBtn);
-    
+
+    return newListItem;
+}
+
+function addListItem(newListItem) {
     outputField.appendChild(newListItem);
 }
 
+function editListItem(id) {
+    //TODO editListItem
+}
+
+function deleteListItem(id) {
+    //TODO deleteListItem
+}
+
+function markAsDoneListItem(id) {
+    //TODO markAsDoneListItem
+}
+
+function loadListItems(todoList) {
+    //TODO loadListItems
+}
+
+function getListValue() {
+    return inputField.value;
+}
+
+function clearListValue() {
+    //TODO clearListValue
+}
+
+//CONTROLLER
+//nur die Functions aus View und Model verwenden
+//keine Elemente aus dem DOM verwenden
+//Controller kennt Model und View 
+function handleAddTodo(listValue) {
+    //Update Model
+    const todoListObj = addTodo(listValue);
+
+    //Update View
+    const newListItem = createNewListItem(todoListObj.id, todoListObj.listValue);
+    addListItem(newListItem);
+}
+
+function handleEditTodo(id) {
+    //TODO handleEditTodo
+}
+
+function handleDeleteTodo(id) {
+    //TODO handleDeleteTodo
+}
+
+function handleMarkAsDoneTodo(id) {
+    //TODO handleMarkAsDoneTodo
+}
+
+function handleSaveTodoList() {
+    //Update Model
+    saveTodoList();
+
+    //Update View
+    //TODO evtl. Meldung anzeigen -> "Daten erfolgreich gespeichert"
+}
+
+function handleLoadTodoList() {
+    //Update Model
+    loadTodoList();
+
+    //Update View
+    loadListItems(todoList);
+}
+
+//Event listener
+function onInputFormSubmit(e) {
+    e.preventDefault();
+
+    const listValue = getListValue();
+    //TODO is listValue not empty?
+    handleAddTodo(listValue);
+
+    console.log(todoList);
+}
+
 //WANN
-addBtn.addEventListener('click', testing);
+inputForm.addEventListener('submit', onInputFormSubmit);
+//TODO addEventListener für saveListBtn und loadListBtn
